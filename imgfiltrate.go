8e6a85ac5981c099be06d2870860f0e0cb8ddfce
Mark Mulder <markmulder@gmail.com>
2016-08-16 10:06:45 +0200

    Remove OCR

--- imgfiltrate.go
+++ imgfiltrate.go
@@ -7,49 +7,22 @@ import (
 	"os"
 
 	"github.com/bittersweet/imgfiltrate/color"
-	"github.com/bittersweet/imgfiltrate/ocr"
 )
 
 type Result struct {
-	ColorPercentage         float64 `json:"color_percentage"`
-	TotalColors             int     `json:"total_colors"`
-	AlphabeticCharacters    int     `json:"alphabetic_characters"`
-	NonAlphabeticCharacters int     `json:"non_alphabetic_characters"`
-	TotalCharacters         int     `json:"total_characters"`
-	DictionaryWords         int     `json:"dictionary_words"`
-	Advice                  string  `json:"advice"`
+	ColorPercentage float64 `json:"color_percentage"`
+	TotalColors     int     `json:"total_colors"`
 }
 
 func processImage(file string) Result {
-	alpha, nonAlpha, totalCharacters, dictionaryWords := ocr.ProcessImage(file)
 	pct, totalColors := color.ProcessImage(file)
 
 	r := Result{
-		ColorPercentage:         pct,
-		TotalColors:             totalColors,
-		AlphabeticCharacters:    alpha,
-		NonAlphabeticCharacters: nonAlpha,
-		TotalCharacters:         totalCharacters,
-		DictionaryWords:         dictionaryWords,
+		ColorPercentage: pct,
+		TotalColors:     totalColors,
 	}
 
 	return r
-}
-
-func (r *Result) advise() {
-	if r.DictionaryWords > 0 {
-		r.Advice = "BAD"
-		return
-	}
-
-	if r.AlphabeticCharacters > r.NonAlphabeticCharacters && r.AlphabeticCharacters > 10 {
-		r.Advice = "BAD"
-		return
-	}
-
-	// TODO: Take ColorPercentage into account
-
-	r.Advice = "GOOD"
 }
 
 func (r *Result) output() {
@@ -64,6 +37,5 @@ func (r *Result) output() {
 func main() {
 	file := os.Args[1]
 	r := processImage(file)
-	r.advise()
 	r.output()
 }
